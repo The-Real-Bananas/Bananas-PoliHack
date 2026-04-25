0c33f814c6a739ab9b3b5aba9be9e638f598e279
AndreeaJigarov <andreea.jigarov@stud.ubbcluj.ro>
2026-04-25 17:44:48 +0300

    sth

--- extension/src/api.ts
+++ extension/src/api.ts
@@ -1,6 +1,6 @@
 import type { DetectionResult } from './types'
 
-export async function scanImage(images: HTMLImageElement[]): Promise<Map<HTMLImageElement, DetectionResult>> {
+export async function scanImages(images: HTMLImageElement[]): Promise<Map<HTMLImageElement, DetectionResult>> {
     const results = new Map<HTMLImageElement, DetectionResult>();
 
     for (const image of images) {
@@ -18,7 +18,7 @@ export async function scanImage(images: HTMLImageElement[]): Promise<Map<HTMLIma
         const result: DetectionResult = await res.json()
         results.set(image, result)
 
-        //results.set(image, { score: 99, source: 'mock' });
+
         } catch(e) {
             console.warn('Failed to scan image:', image.src, e)
         }
