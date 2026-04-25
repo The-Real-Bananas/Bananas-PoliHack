6955d48ef462a9bd1c1d1656ddfd8bf249547419
AndreeaJigarov <andreea.jigarov@stud.ubbcluj.ro>
2026-04-25 17:23:58 +0300

    sth

--- extension/src/api.ts
+++ extension/src/api.ts
@@ -8,15 +8,21 @@ export async function scanImage(images: HTMLImageElement[]): Promise<Map<HTMLIma
             continue;
         }
 
-        /*const res = await fetch('http://localhost:8000/detect/image', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ url: image.src })
-        })
+        try{
+            const res = await fetch('http://localhost:8000/detect/image', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ url: image.src })
+            })
 
         const result: DetectionResult = await res.json()
-        results.set(image, result)*/
-        results.set(image, { score: 99, source: 'mock' });
+        results.set(image, result)
+
+        //results.set(image, { score: 99, source: 'mock' });
+        } catch(e) {
+            console.warn('Failed to scan image:', image.src, e)
+        }
+
     }
 
   return results;
