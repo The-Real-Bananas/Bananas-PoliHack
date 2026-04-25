29e6c7458a5ec8875e46fea4b3ae6c875abbc395
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-04-25 12:57:41 +0300

    Published extension setup

---
+++ extension/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        content: 'src/content.ts',
+        background: 'src/background.ts',
+        popup: 'popup.html',
+      },
+      output: {
+        entryFileNames: '[name].js',
+      }
+    }
+  }
+})
